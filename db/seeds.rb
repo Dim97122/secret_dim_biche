ba597a66fae1c009e60ff063b0b232990c4fbd38
Dim <dimitri.kiavue@gmail.com>
2018-08-02 14:23:24 +0200

    Initial Commit

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+require 'faker'
+
+  user1 = User.create!(email: 'jean@gmail.com', password: 'poulet', password_confirmation: 'poulet')
+  user2 = User.create!(email: 'paul@gmail.com', password: 'poulet', password_confirmation: 'poulet')
+  user3 = User.create!(email: 'marc@gmail.com', password: 'poulet', password_confirmation: 'poulet')
+  user4 = User.create!(email: 'matthieu@gmail.com', password: 'poulet', password_confirmation: 'poulet')
+  user5 = User.create!(email: 'kwevan@gmail.com', password: 'poulet', password_confirmation: 'poulet')
